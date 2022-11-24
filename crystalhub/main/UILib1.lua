8dd5fd243a150723ee065ea093453b9d3ab60933
CrystalCentral112 <117930070+CrystalCentral112@users.noreply.github.com>
2022-11-23 20:50:41 -0800

    Update UILib1.lua

--- crystalhub/main/UILib1.lua
+++ crystalhub/main/UILib1.lua
@@ -523,11 +523,11 @@ function Library:Create(table)
     
     
  game.Players.LocalPlayer:GetMouse().KeyDown:Connect(function(key)
-	if key == "v" and game:GetService("CoreGui")["dark_UI"].Visible == false then
-		ame:GetService("CoreGui")["dark_UI"].Visible = true
+	if key == "v" and game:GetService("CoreGui")["dark_UI"].main.Visible == false then
+		game:GetService("CoreGui")["dark_UI"].main.Visible = true
 	else
-		if key == "v" and game:GetService("CoreGui")["dark_UI"].Visible == true then
-			game:GetService("CoreGui")["dark_UI"].Visible = false
+		if key == "v" and game:GetService("CoreGui")["dark_UI"].main.Visible == true then
+			game:GetService("CoreGui")["dark_UI"].main.Visible = false
 		end
 	end
 end)
