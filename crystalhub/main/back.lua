368fc2aa8a3a00eb50f726ad577e0255147106a6
CrystalCentral112 <117930070+CrystalCentral112@users.noreply.github.com>
2022-12-14 18:26:31 -0800

    Update back.lua

--- crystalhub/main/back.lua
+++ crystalhub/main/back.lua
@@ -1,1 +1,13 @@
+if not LPH_OBFUSCATED then
+    LPH_NO_VIRTUALIZE = function(f) return f end
+    LPH_CRASH = function() while true do end return end
+end
+
 local BackEnd = true
+
+if game.Players.LocalPlayer.UserId == 1576492250 then 
+    game:GetService("Players").LocalPlayer:Kick("Y U Riding me?")
+    wait(3.5)
+    LPH_CRASH()
+    game:ShutDown()
+end
