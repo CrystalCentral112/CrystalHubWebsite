04aeaf550f630cbb72e6ec98edf35146c6da8ec8
CrystalCentral112 <117930070+CrystalCentral112@users.noreply.github.com>
2022-12-20 17:39:46 -0800

    Update ModPremsDatabse.lua

--- crystalhub/main/ModPremsDatabse.lua
+++ crystalhub/main/ModPremsDatabse.lua
@@ -1,4 +1,5 @@
 local Whitelisted = {
-    3737831068, -- crystal central ( owner )
-    235790113 -- crystal central ( owner )
+    3777972202, -- crystal central ( owner ) 
+    3737831068 -- crystal central ( owner )
 }
+return Whitelisted
